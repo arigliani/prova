4ad7aa0e3fbf9d3c2fda17481d4eb3806a424c88
arigliani <gianni.arigliani@gmail.com>
2017-05-15 22:50:13 +0200

    es 8.1

---
+++ 8.js
@@ -0,0 +1,20 @@
+
+
+function risultato(){
+	a=document.f.frt.value;
+    var re=/^(\+|-|^)(\d+\.\d{2}$)/;
+        
+	if((a== "somma")&&(re.test(document.f.primo.value))){
+		document.f.somma.value=parseFloat(document.f.primo.value)+parseFloat(document.f.secondo.value);
+	}
+	if(a== "differenza"){
+		document.f.somma.value=parseFloat(document.f.primo.value)-parseFloat(document.f.secondo.value);
+	}
+	if(a== "prodotto"){
+		document.f.somma.value=parseFloat(document.f.primo.value)*parseFloat(document.f.secondo.value);
+	}
+	if(a== "quoziente"){
+		document.f.somma.value=parseFloat(document.f.primo.value)/parseFloat(document.f.secondo.value);
+	}
+	
+}
